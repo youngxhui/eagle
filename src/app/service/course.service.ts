ca8f5da335f1116610496b1cf2b01525767eb679
youngxhui <youngxhui@gmail.com>
2021-01-12 19:42:55 +0800

    课程列表

--- src/app/service/course.service.ts
+++ src/app/service/course.service.ts
@@ -1,35 +1,39 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 import Result from '../entity/result';
-import { Observable } from 'rxjs';
-import { Tip } from '../entity/tip';
-import { SubTip } from '../entity/subTip';
-import { Course } from '../entity/course';
+import {Observable} from 'rxjs';
+import {Tip} from '../entity/tip';
+import {SubTip} from '../entity/subTip';
+import {Course} from '../entity/course';
 import Page from '../entity/page';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class CourseService {
 
-    constructor(private httpClient: HttpClient) {
-    }
+  constructor(private httpClient: HttpClient) {
+  }
 
-    getTip(): Observable<Result<Array<Tip>>> {
-        return this.httpClient.get<Result<Array<Tip>>>('/tip/all');
-    }
+  getTip(): Observable<Result<Array<Tip>>> {
+    return this.httpClient.get<Result<Array<Tip>>>('/tip/all');
+  }
 
-    getSubTipByTipId(tipId: number): Observable<Result<Array<SubTip>>> {
-        return this.httpClient.get<Result<Array<SubTip>>>(`/subtip/list/enable/${tipId}`);
-    }
+  getSubTipByTipId(tipId: number): Observable<Result<Array<SubTip>>> {
+    return this.httpClient.get<Result<Array<SubTip>>>(`/subtip/list/enable/${tipId}`);
+  }
 
-    getCourseByTip(tipId: number, page: number, size: number): Observable<Result<Page<Course>>> {
-        return this.httpClient.get<Result<Page<Course>>>(`/course/tip/${tipId}?page=${page}&size=${size}`);
-    }
+  getCourseByTip(tipId: number, page: number, size: number): Observable<Result<Page<Course>>> {
+    return this.httpClient.get<Result<Page<Course>>>(`/course/tip/${tipId}?page=${page}&size=${size}`);
+  }
 
 
-    getCourseById(courseId: number): Observable<Result<Course>> {
-        return this.httpClient.get<Result<Course>>(`/course/${courseId}`);
-    }
+  getCourseById(courseId: number): Observable<Result<Course>> {
+    return this.httpClient.get<Result<Course>>(`/course/${courseId}`);
+  }
+
+  getAllCourse(page: number, size: number): Observable<Result<Page<Course>>> {
+    return this.httpClient.get<Result<Page<Course>>>(`/course/list?page=${page}&size=${size}`);
+  }
 
 }
